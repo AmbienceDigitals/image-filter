5a610004d7384054cac8941d2641e8f90c424058
Ayano Ezekiel <52824649+AmbienceDigitals@users.noreply.github.com>
2022-06-16 21:33:26 +0100

    reviewed

--- src/filteredImage/routes/filteredImage.router.ts
+++ src/filteredImage/routes/filteredImage.router.ts
@@ -1,48 +1,12 @@
 import { Router, Request, Response } from 'express';
-import {filterImageFromURL, deleteLocalFiles} from '../../util/util';
+
 
 
 const router: Router = Router();
 
-// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMETERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
 
-router.get('/filteredimage/', 
-async (req: Request, res: Response) => {
-    const {image_url} = req.query;
-// using try/catch for error handling
-    try {
-            // checking if image query exists
-    if (!image_url) {
-        return res.status(400).send('no image url detected');
-    }
 
-    // save image_url to a variable
-    const filteredPath = await filterImageFromURL(image_url)
-
-    // using res.sendfile to return the file obtained from the filteredpath
-    return res.status(200).sendFile(filteredPath, () => {
-        // delete file once thw operation is successfully completed
-                deleteLocalFiles([filteredPath]);
-    })
-    } catch (error) {
-        return res.status(500).send("There is an error with the link provided")
-    }
-
-})
 
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {Router, Request, Response} from 'express'
-import { FilteredImageRouter } from './filteredImage/routes/filteredImage.router';
+import {Request, Response} from 'express'
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
 
   // Init the Express application
   const app = express();
-
-  // const router: Router = Router();
 
   // Set the network port
   const port = process.env.PORT || 8082;
@@ -19,11 +17,49 @@ import { FilteredImageRouter } from './filteredImage/routes/filteredImage.router
       // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("Welcome to the Filtered Image Microservice Api \n To use this application, navigate to /api/v0/image_url?image_url=your image URL")
+    res.send("Welcome to the Filtered Image Microservice Api")
   } );
 
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMETERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
 // route to filtered image
-  app.use('/api/v0', FilteredImageRouter)
+app.get('/filteredimage/', 
+async (req: Request, res: Response) => {
+    const {image_url} = req.query;
+// using try/catch for error handling
+    try {
+            // checking if image query exists
+    if (!image_url) {
+        return res.status(400).send('no image url detected');
+    }
+
+    // save image_url to a variable
+    const filteredPath = await filterImageFromURL(image_url)
+
+    // using res.sendfile to return the file obtained from the filteredpath
+    return res.status(200).sendFile(filteredPath, () => {
+        // delete file once thw operation is successfully completed
+                deleteLocalFiles([filteredPath]);
+    })
+    } catch (error) {
+        return res.status(500).send("There is an error with the link provided")
+    }
+
+})
   
 
   // Start the Server
