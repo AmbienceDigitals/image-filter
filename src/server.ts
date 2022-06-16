6f3d9ab3a66a32f5a08a1ebaadf2a75750d02b96
Ayano Ezekiel <52824649+AmbienceDigitals@users.noreply.github.com>
2022-06-16 18:19:36 +0100

    finalized

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ import { FilteredImageRouter } from './filteredImage/routes/filteredImage.router
 
   // Init the Express application
   const app = express();
-  const users = [];
 
   // const router: Router = Router();
 
@@ -16,48 +15,14 @@ import { FilteredImageRouter } from './filteredImage/routes/filteredImage.router
   
   // Use the body parser middleware for post requests
     app.use(bodyParser.json());
-    app.set('view engine', 'ejs');
-    app.use('/assets', express.static('assets'));
-    app.use(express.urlencoded({extended: false}));
 
       // Root Endpoint
   // Displays a simple message to the user
-  // app.get( "/", async ( req, res ) => {
-  //   res.send("Welcome to the Filtered Image Microservice Api \n try GET /filteredimage?image_url={{}}")
-  // } );
+  app.get( "/", async ( req, res ) => {
+    res.send("Welcome to the Filtered Image Microservice Api \n To use this application, navigate to /api/v0/image_url?image_url=your image URL")
+  } );
 
-    app.get('/register', (req, res) => {
-          res.render('register.ejs');
-      });
-
-    app.get('/login', (req, res) => {
-        res.render('login.ejs');
-    });
-
-    app.post('/register', (req, res) => {
-      try {
-          users.push({
-              id: Date.now().toString(),
-              name: req.body.name,
-              password: req.body.password,
-              email: req.body.email
-          })
-          res.redirect('/login');
-      } catch {
-          res.redirect('/register');
-      }
-    });
-
-    app.post('/login', (req, res) => {
-      try {
-          
-      } catch {
-          
-      }
-      res.render('index.ejs', {data: req.body});
-    });
-
-
+// route to filtered image
   app.use('/api/v0', FilteredImageRouter)
   
 
